5e5f0da3590a112866a6de99d9546e4decfb5360
RHSvenson <konto@rhsvenson.dk>
2025-07-30 11:38:07 +0200

    Added sprite functionality to component

--- Content.Shared/_Starlight/Armor/Upgrades/Components/ArmorUpgradeComponent.cs
+++ Content.Shared/_Starlight/Armor/Upgrades/Components/ArmorUpgradeComponent.cs
@@ -4,6 +4,7 @@ using Content.Shared.Tag;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Armor.Upgrades.Components;
 
@@ -31,9 +32,25 @@ public sealed partial class ArmorUpgradeComponent : Component
     /// </summary>
     [DataField]
     public TitleChangeType? TitleChanges;
+
+    /// <summary>
+    /// If the upgradeable armor has a sprite state corresponding to this string,
+    /// set that state to visible.
+    /// </summary>
+    [DataField]
+    public string? RevealLayerName;
+
+    /// <summary>
+    /// If set, will apply the specified layer to ALL armor pieces.
+    /// TODO: Make this discriminate between Vox and other race-specific sprites.
+    /// </summary>
+    /// <remarks>
+    /// The reason for specifying sprite-location is for clothing with
+    /// toggleable components, such as hardsuits.
+    [DataField]
+    public Dictionary<SpriteLocation, SpriteSpecifier>? OverlaySprites = new();
 }
 
-[RegisterComponent]
 [DataDefinition]
 public sealed partial class TitleChangeType
 {
@@ -48,4 +65,20 @@ public sealed partial class TitleChangeType
     /// </summary>
     [DataField]
     public string? Prefix = null;
+}
+
+public enum SpriteLocation
+{
+    Universal = 0,
+    Back = 1,
+    Belt = 2,
+    Ears = 3,
+    Eyes = 4,
+    Hands = 5,
+    Head = 6,
+    Mask = 7,
+    Neck = 8,
+    OuterClothing = 9,
+    Shoes = 10,
+    Under = 11,
 }
